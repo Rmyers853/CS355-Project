44b1dc7ec8666ce41ad7bb13c3eaf446eb15950e
Rmyers853 <myers442@purdue.edu>
2023-11-14 18:52:18 -0500

    Generated Alice/Bob 500 MB Files

---
+++ generateFiles.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+using namespace std;
+
+int getFileLength(string fileName) {
+  ifstream in_file(fileName, ios::binary);
+  in_file.seekg(0, ios::end);
+  return in_file.tellg();
+}
+
+int main() {
+  for (int i = 1; i < 11; i++) {
+    string fileName = "Alice_file" + to_string(i) + ".txt";
+    if (i > 5) {
+      fileName = "Bob_file" + to_string(i-5) + ".txt";
+    }
+    //ifstream MyFile(fileName);
+    //stringstream buffer;
+    //buffer << MyFile.rdbuf();
+    //cout<<buffer.str()<<endl;
+    int file_size = getFileLength(fileName);
+    int previous_file_size = file_size;
+    cout<<"Size of the file is"<<" "<< file_size<<" "<<"bytes"<<endl;
+    ofstream MyFile;
+    MyFile.open(fileName, ios::app);
+    char randomChar = '!';
+    unsigned seed= time(0);
+    srand(seed);
+    cout<<randomChar<<endl;
+    while (file_size < 500000000) {
+      for (int i = 0; i < 1000; i++) {
+        randomChar = '!' + rand()%93;
+        MyFile << randomChar;
+      }
+      file_size = getFileLength(fileName);
+      if (previous_file_size != file_size) {
+        cout<<"Size of the file is"<<" "<< file_size<<" "<<"bytes"<<endl;
+        previous_file_size = file_size;
+      }
+    }
+    MyFile.close();
+  }
+}
